763aa16d02cd8427c5b2675d476f717a25889154
EdvaldoCariege <osvaldocariege06@gmail.com>
2023-06-14 07:46:19 +0100

    First commit

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <header className="flex items-center justify-between mb-8 w-full">
+      <h2 className="">Dashboard</h2>
+      <h2 className="">Welcome back, Osvaldo</h2>
+    </header>
+  )
+}
---
+++ src/app/components/TopCards.tsx
@@ -0,0 +1,9 @@
+export function TopCards() {
+  return (
+    <section className="grid lg:grid-cols-5 gap-4 p-4">
+      <div className="lg:col-span-2 bg-white flex justify-between w-full border p-4 rounded-lg"></div>
+      <div className="lg:col-span-2 bg-white flex justify-between w-full border p-4 rounded-lg"></div>
+      <div className="bg-white flex justify-between w-full border p-4 rounded-lg"></div>
+    </section>
+  )
+}
