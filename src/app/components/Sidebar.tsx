dccc7e16460ca17b599bb2a473b8376b54a02df4
EdvaldoCariege <osvaldocariege06@gmail.com>
2023-06-15 08:34:10 +0100

    Sidebar

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -8,14 +8,14 @@ export function Sidebar() {
   return (
     <aside className="h-screen p-4 bg-white border-r-[1px] flex flex-col justify-between">
       <div className="flex flex-col items-center">
-        <Link href={'/'}>
+        <Link href={'/'} className={'hidden'}>
           <div className="bg-purple-800 text-white p-3 rounded-lg inline-block">
             <RxSketchLogo  size={20} />
           </div>
         </Link>
         <span className="border-b-[1px] border-gray-200 w-full p-2"></span>
         <Link href={'/'}>
-          <div className="bg-gray-100 hover:bg-gray-100 cursor-pointer my-4 p-3 rounded-lg inline-block">
+          <div className="bg-purple-800  text-white cursor-pointer my-4 p-3 rounded-lg inline-block">
             <RxDashboard  size={20} />
           </div>
         </Link>
@@ -29,7 +29,7 @@ export function Sidebar() {
             <HiOutlineShoppingBag  size={20} />
           </div>
         </Link>
-        <Link href={'/'}>
+        <Link href={'/'} className={'hidden'}>
           <div className="bg-gray-100 hover:bg-gray-100 cursor-pointer my-4 p-3 rounded-lg inline-block">
             <FiSettings  size={20} />
           </div>
